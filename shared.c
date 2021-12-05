29f906996013d9ab7e674e027e9de9ff79dfe933
Nivas Muthu M G <nivasmuthu452@gmail.com>
2021-12-05 15:24:40 +0000

    Added Bunch of Programs

--- shared.c
+++ shared.c
@@ -1,3 +1,5 @@
+//Sender.c
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -17,4 +19,20 @@ int main(){
 	strcpy(shared_memory, buff);
 	printf("Data: %s\n", (char *)shared_memory);
 }
+
+//Receiver.c
+#include<stdio.h>
+#include<sys/shm.h>
+#include<string.h>
+#include<unistd.h>
+
+int main(){
+	char *shared_memory;
+	int shmid = shmget((key_t)6969, 1024, 0666);
+	printf("shmid is %d\n", shmid);
+	shared_memory = shmat(shmid, NULL, 0);
+	printf("Process attached at %p\n", shared_memory);
+	printf("Data read from shared memory is: %s", (char *)shared_memory); 
+}
+
 
